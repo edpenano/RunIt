796d45df1b07babf4b37ef941c9e497ed7e76dd1
Ed Penano <hi@edpenano.com>
2023-12-04 11:15:10 -0800

    Initial Commit

---
+++ RunIt/RunItApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunItApp.swift
+//  RunIt
+//
+//  Created by Ed Penano on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct RunItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
